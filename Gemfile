6ba695822135330ce81f911d1d3778052fb4942c
Aaron Gibralter <aaron.gibralter@gmail.com>
2011-12-06 02:24:22 -0500

    Ok...

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails',      '3.1.3'
+gem 'haml',       '3.1.4'
+gem 'sass-rails', '3.1.5'
+gem 'compass',    '0.12.alpha.2'
+gem 'oily_png',   '1.0.2'
+gem 'chunky_png', '1.2.5'
+
+group :assets do
+  gem 'coffee-rails',       '3.1.1'
+  gem 'jquery-rails',       '1.0.19'
+  gem 'uglifier',           '1.1.0'
+  gem 'compass-960-plugin', '0.10.4', require: 'ninesixty'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassRailsUrlHelpersTest::Application.config.secret_token = '0487bf6a464bc476356c5dc906c26c492b4dbe32fc564c12e95a6b376edbc3e43d0ace2c27ab54a8b48083e7951619e2c8f604d7ca30be89ae4decdf37710ee7'
